f5760f57d951953bbdd75af0931ae415889e4f01
GuyHarperDay <guy.harper-day@bbc.co.uk>
2021-11-20 15:02:38 +0000

    Adding ga and upgrading dependencies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import StandardIndex from 'components/views/StandardIndex';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('G-7YYLW5J4CR');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 const App = () => {
   return (
