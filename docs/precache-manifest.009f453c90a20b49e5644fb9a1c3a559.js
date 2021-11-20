fa78787003b91fe3cd91563f388cb30382b9ec1e
GuyHarperDay <kingsofwartools@users.noreply.github.com>
2021-11-20 14:12:03 +0000

    New build, triggered by change to the master branch.

---
+++ docs/precache-manifest.009f453c90a20b49e5644fb9a1c3a559.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06b7de6095067e93496c7e798dd3b349",
+    "url": "/listbuilder/index.html"
+  },
+  {
+    "revision": "59043820ab2bc4532d60",
+    "url": "/listbuilder/static/css/main.871bfe3c.chunk.css"
+  },
+  {
+    "revision": "18879e529d57cc50bddf",
+    "url": "/listbuilder/static/js/2.258c6628.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/listbuilder/static/js/2.258c6628.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59043820ab2bc4532d60",
+    "url": "/listbuilder/static/js/main.e998d061.chunk.js"
+  },
+  {
+    "revision": "9383e4f5d95f35205faf",
+    "url": "/listbuilder/static/js/runtime-main.6ba375da.js"
+  }
+]);
