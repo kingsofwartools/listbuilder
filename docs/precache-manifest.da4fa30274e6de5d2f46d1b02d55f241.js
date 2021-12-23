8248d118ce2c02cf5645ec7180a65e298650a4b5
GuyHarperDay <GuyHarperDay@users.noreply.github.com>
2021-12-23 09:52:24 +0000

    New build, triggered by change to the master branch.

---
+++ docs/precache-manifest.da4fa30274e6de5d2f46d1b02d55f241.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3a39b988f6b3fc237a540b9d9555716",
+    "url": "/listbuilder/index.html"
+  },
+  {
+    "revision": "8cd18fda7568ad33c8c7",
+    "url": "/listbuilder/static/css/main.0b6d0a8e.chunk.css"
+  },
+  {
+    "revision": "b60dd80c1f87cdfe9471",
+    "url": "/listbuilder/static/js/2.74618d60.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/listbuilder/static/js/2.74618d60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cd18fda7568ad33c8c7",
+    "url": "/listbuilder/static/js/main.5b092661.chunk.js"
+  },
+  {
+    "revision": "a14de33e3942f906ea5e",
+    "url": "/listbuilder/static/js/runtime-main.ef274996.js"
+  }
+]);
